806b7f24dd3161b55ff8f1af375c5b0f04136216
Miliya-Ai <102935805+Miliya-Ai@users.noreply.github.com>
2022-10-13 15:07:45 -0400

    Add files via upload

---
+++ BombExploder/src/lireFichier.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+
+public class lireFichier{
+    public static void main(String args[]) {
+        try{
+            FileInputStream fin = new FileInputStream("src/sample.txt");
+            DataInputStream reader = new DataInputStream(fin);
+            BufferedReader buffReader = new BufferedReader(new InputStreamReader(reader));
+            String strTmp;
+            while((strTmp = buffReader.readLine())!=null){
+                System.out.println(strTmp);
+            }
+            buffReader.close();
+        } catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
