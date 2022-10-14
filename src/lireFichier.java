c52619ec6bd7ea02df3e1aadea8f30f3983cd62c
Miliya-Ai <102935805+Miliya-Ai@users.noreply.github.com>
2022-10-13 22:21:46 -0400

    fixé merge

--- src/lireFichier.java
+++ src/lireFichier.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-<<<<<<< HEAD
+
 public  class lireFichier {
     private final String filename;
 
@@ -15,15 +15,10 @@ public  class lireFichier {
 
     public static List<String> LireFichier(String nom) {
         ArrayList<String> result = new ArrayList<>();
-
-        try {
-            FileInputStream fin = new FileInputStream(nom);
-=======
-public class lireFichier{
-    public static void main(String[] args) {
+        
         try{
             FileInputStream fin = new FileInputStream("src/sample.txt");
->>>>>>> c57aa6cda394834b4d4dfdbdee006055ed1a0638
+
             DataInputStream reader = new DataInputStream(fin);
             BufferedReader buffReader = new BufferedReader(new InputStreamReader(reader));
             String strTmp;
